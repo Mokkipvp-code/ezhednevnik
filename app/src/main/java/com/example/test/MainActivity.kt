5a7bc58ce210e21bc0e08c5663e006321f45ba73
Mokkipvp-code <mokkipvp@gmail.com>
2025-11-13 21:57:11 +0300

    SecondCommit

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "ывлаопфыиорфыад Саня $name!",
+        text = "Привет Саня $name!",
         modifier = modifier
     )
 }
