3a9e061435b73e959793ca08586cccb3a2d9cfca
Klounada2025hack <dark_0n@mail.ru>
2025-11-13 22:02:08 +0300

    yeyqyeqy

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Привет кукукукукукуреку Саня $name!",
+        text = "Привет кукукукукукурекуsafsfasfaf Саня $name!",
         modifier = modifier
     )
 }
